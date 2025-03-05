a935cc01f177e2a8b1e1cbcd41d4dd2ef4b37d4e
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2025-03-05 11:21:49 +0530

    adding transport test file

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,9 +1,23 @@
 package p2p
-import "net"
+
+import (
+	"net"
+	"sync"
+)
 
 type TCPTransport struct {
-	//make configurations 
+	//make configurations
 	listenAddress string
-	listener  net.Listender
-	peers map[net.Addr ]
+	listener      net.Listener
+	// mutex for peer lock
+	mu sync.RWMutex
+	// put your mutex above the thing you want to protect it
+	peers map[net.Addr]Peer
+}
+
+func NewTCPTransport(listenAddr string) *TCPTransport {
+	return &TCPTransport{
+		listenAddress: listenAddr,
+	}
+
 }
---
+++ p2p/tcp_transport_test.go
@@ -0,0 +1,9 @@
+package p2p
+
+import "testing"
+
+func TestTCPTransport(t *testing.T) {
+	listenAddr := ":4000"
+	tr := NewTCPTransport()
+
+}
