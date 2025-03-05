617078b40f6c8ae6cffd460e4db98f882c30a52a
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-03-05 10:22:07 +0530

    adding makefile

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-type FileServer struct {}
-
-
-
-func (fs *FileServer) start() {
-  ln,err := net.Listen("tcp",":3000")
-  if err !=nil {
-    log.Fatal(err)
-  }
-
-  for {
-    conn, err := ln.Accept()
-    if err != nil {
-      log.Fatal(err)
-    }
-  }
-}
-
-
-
-  func (fs *FileServer ) readLoop() {
-    buf := make([] byte , 2048)
-    for {
-      n, err :=
-  }
-
-
-  }
+import "fmt"
 
 func main() {
-
+	fmt.Println("We gucci")
 }
